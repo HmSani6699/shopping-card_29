d5628491d18a94d73369c07f80a301160aca529b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-15 10:28:03 +0600

    complete the all website

--- js/shopping.js
+++ js/shopping.js
@@ -1,23 +1,23 @@
 // increment the input value
-function setInputPlusValue(id) {
-    const phoneInputFiled = document.getElementById(id);
+function setInputPlusValue(id, num) {
+    const phoneInputFiled = document.getElementById(id + 'input');
     const phoneInputFiledValueString = phoneInputFiled.value;
     let phoneInputValue = parseInt(phoneInputFiledValueString);
     phoneInputFiled.value = phoneInputValue += 1;
-    const newElementValue = phoneInputFiled.value * 1219;
-    setElementValue(newElementValue)
+    const newElementValue = phoneInputFiled.value * num;
+    setElementValue(id, newElementValue)
 }
 
 // decrement the input value
-function setInputMinusValue(id) {
-    const phoneInputFiled = document.getElementById(id);
+function setInputMinusValue(id, num) {
+    const phoneInputFiled = document.getElementById(id + 'input');
     const phoneInputFiledValueString = phoneInputFiled.value;
     let phoneInputValue = parseInt(phoneInputFiledValueString);
     let newInputValue = phoneInputValue -= 1;
-    if (newInputValue > 0) {
+    if (newInputValue >= 0) {
         phoneInputFiled.value = newInputValue;
-        let totalCost = phoneInputFiled.value * 1219
-        setElementValue(totalCost)
+        let totalCost = phoneInputFiled.value * num
+        setElementValue(id, totalCost)
     }
     else {
         alert('please provide the positive number')
@@ -25,14 +25,52 @@ function setInputMinusValue(id) {
 }
 
 // set the new balance 
-function setElementValue(value) {
-    document.getElementById('phone-total').innerText = value
+function setElementValue(id, value) {
+    document.getElementById(id + 'total').innerText = value
 }
 
+//get the element
+function getElementValue(id) {
+    const totalPhonePrice = document.getElementById(id).innerText;
+    return totalPhonePrice
+}
+
+
+//phone part
 document.getElementById('phone-plus').addEventListener('click', function () {
-    setInputPlusValue('phone-input');
+    setInputPlusValue('phone-', 1219);
+    subTotal()
 });
 
 document.getElementById('phone-minus').addEventListener('click', function () {
-    setInputMinusValue('phone-input')
+    setInputMinusValue('phone-', 1219);
+    subTotal()
 })
+
+//case part
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    setInputPlusValue('case-', 59);
+    subTotal()
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    setInputMinusValue('case-', 59);
+    subTotal()
+});
+
+
+//subtotal
+function subTotal() {
+    const totalPhonePrice = getElementValue('phone-total');
+    const totalCasePrice = getElementValue('case-total');
+    let subTotal = parseInt(totalPhonePrice) + parseInt(totalCasePrice);
+    setElementValue('sub-', subTotal);
+
+    let totalTex = subTotal * 0.1;
+    let totalTexNumber = parseFloat(totalTex.toFixed(1));
+    setElementValue('tex-', totalTexNumber);
+
+    let total = subTotal + totalTexNumber;
+    setElementValue('price-', total);
+}
